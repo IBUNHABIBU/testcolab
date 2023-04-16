151e5c5d83b54010d183415454273abd34db021c
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-17 01:22:57 +0300

    Add navbar

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+const list = [
+  {
+    id: 1,
+    name: 'Intro',
+  },
+  {
+    id: 2,
+    name: 'Portfolio',
+  },
+  {
+    id: 3,
+    name: 'Contact',
+  },
+  {
+    id: 4,
+    name: 'Blog',
+  },
+];
+
+export default list;
