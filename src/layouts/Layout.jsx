925cb5168f236945ab2a81221cc274e38adf74b5
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-17 01:25:02 +0300

    Define the higher order component

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+const Layout = (Component) => ({ ...props }) => (
+    <div className="layout">
+      <Nav />
+      <div className="container">
+        <div className="main-container">
+          <Component {...props} />
+        </div>
+      </div>
+    </div>
+  );
+  
+  export default Layout;
