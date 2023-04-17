b7322e32f7b2a3b1033dc1b554bdb0d8b579b016
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-17 21:45:02 +0300

    Add react router dom

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,19 +1,28 @@
 const list = [
   {
     id: 1,
+    name: 'Home',
+    path: '/'
+  },
+  {
+    id: 1,
     name: 'Intro',
+    path: '/intro'
   },
   {
     id: 2,
     name: 'Portfolio',
+    path: '/portfolio'
   },
   {
     id: 3,
     name: 'Contact',
+    path: '/contact'
   },
   {
     id: 4,
     name: 'Blog',
+    path: '/blog'
   },
 ];
 
