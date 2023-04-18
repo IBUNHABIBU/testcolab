21ed9ce7016d5326ebeb51bb4dbfde9433155a97
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 16:47:56 +0300

    Add popup modal

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Admin = () => (
+const Admin = () => {
+    const [modalOpen, setmodalOpen] = useState(false);
+    const handleClick = 
+    return(
   <div className="admin">
     <h2>Admin Panel</h2>
     <div className="admin__buttons">
-      <button className="btn skills" type="submit">Add Skills</button>
+      <button className="btn skills" type="submit" onClick={handleClick}>Add Skills</button>
       <button className="btn project" type="submit">Add Project</button>
     </div>
   </div>
-);
+)
+};
 
 export default Admin;
