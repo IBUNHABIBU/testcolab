cf28b6ec77a68f9842491acb8f8461f960cdf30d
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 10:52:52 +0300

    Add login component

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,19 @@ import Contact from './routes/Contact';
 import Home from './routes/Home';
 // import App from './App';
 import './app.scss';
+import Login from './routes/Login';
 
 const AboutComponent = layout(About);
 const ContactComponent = layout(Contact);
 const HomeComponent = layout(Home);
+const LoginComponent = layout(Login);
 
 const router = createBrowserRouter(
   [
     { path: '/', element: <HomeComponent /> },
     { path: '/contact', element: <ContactComponent /> },
     { path: '/about', element: <AboutComponent /> },
+    { path: '/login', element: <LoginComponent />}
   ],
 );
 ReactDOM.createRoot(document.getElementById('root')).render(
