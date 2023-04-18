627f01f3098a54d65271a2a5d9c9bc120a1d9ce7
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 16:00:27 +0300

    Style the admin panel

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -4,7 +4,7 @@ const Admin = () => (
   <div className="admin">
     <h2>Admin Panel</h2>
     <div className="admin__buttons">
-      <button className="btn skills" type="submit">Add Skill</button>
+      <button className="btn skills" type="submit">Add Skills</button>
       <button className="btn project" type="submit">Add Project</button>
     </div>
   </div>
