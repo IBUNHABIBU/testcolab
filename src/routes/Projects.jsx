48063fb740433673ca2ae374967ebe6adcf5615f
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 15:02:27 +0300

    Style the footer

---
+++ src/routes/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+        <Headers />
+        <Login />
+        <Footer />
+    </div>
+  )
+}
+
+export default Projects
