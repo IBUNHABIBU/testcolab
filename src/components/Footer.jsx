536171541c4c3c602129d28b10633030f265f175
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 14:48:10 +0300

    Style the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
     <div className="footer__social">
       <a href="https://github.com/IBUNHABIBU" target="_blank" rel="noreferrer"><FaGithub /></a>
       <a href="https://twitter.com/ibunhabibu" target="_blank" rel="noreferrer"><FaTwitter /></a>
-      <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+      <a href="https://linkedin.com/in/salum-habibu" target="_blank" rel="noreferrer"><FaLinkedin /></a>
     </div>
     <div className="footer__contact">
       <span>Phone: 555-555-5555</span>
