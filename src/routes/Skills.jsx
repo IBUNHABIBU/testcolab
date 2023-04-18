736749c6d8c6a86aa310c5543cba67bb54ce9035
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 15:26:06 +0300

    Add Skills page

--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Skills = () => (
   <div className='skills'>
-
+<h2 className="skills__title">Skills</h2>
+      <ul className="skills__list">
+        <li className="skills__list__item">HTML</li>
+        <li className="skills__list__item">CSS</li>
+        <li className="skills__list__item">JavaScript</li>
+        <li className="skills__list__item">React</li>
+        <li className="skills__list__item">Node.js</li>
+      </ul>
   </div>
 );
 
