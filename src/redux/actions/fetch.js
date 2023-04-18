40e564a19801893bca019027ac950634765a77db
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-19 02:52:56 +0300

    Call the post request to create Project

--- src/redux/actions/fetch.js
+++ src/redux/actions/fetch.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 import {
-    addProject
-  } from './actions';
+  addProject,
+} from './actions';
 // export const urlBase = 'https://colapi.darlive.cyou';
 export const urlBase = 'http://localhost:3000/api/v1/projects';
 
 export const createProject = () => async (dispatch) => {
-    const response = await axios.get(`${urlBase}/api/v1/projectss`, {
-      withCredentials: true,
-    });
-    dispatch(addProject(response.data));
-  };
+  const response = await axios.get(`${urlBase}/api/v1/projectss`, {
+    withCredentials: true,
+  });
+  dispatch(addProject(response.data));
+};
