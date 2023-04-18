e922406cd41b19da6fa388d9575422a31f630ccc
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 17:00:02 +0300

    Style the popup modal

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 
 const Admin = () => {
-  const [modalOpen, setmodalOpen] = useState(false);
+  const [skillsModal, setSkillsModal] = useState(false);
+  const [projectModal, setProjectModal] = useState(false);
   const handleClick = () => {
-    setmodalOpen(true);
+    setSkillsModal(true);
   };
 
   const handleCloseModal = () => {
-    setmodalOpen(false);
-  }
+    setSkillsModal(false);
+  };
+  const handleCloseProject = () => {
+    setProjectModal(false);
+  };
   return (
     <div className="admin">
       <h2>Admin Panel</h2>
@@ -17,10 +21,27 @@ const Admin = () => {
         <button className="btn project" type="submit">Add Project</button>
       </div>
 
-      {modalOpen && (
+      {skillsModal && (
         <div className="modal">
           <div className="modal__content">
             <span className="close" onClick={handleCloseModal}>&times;</span>
+            <h3>Add Skill</h3>
+            <form>
+              <label htmlFor="skill-name">Skill Name:</label>
+              <input type="text" id="skill-name" name="skill-name" />
+
+              <label htmlFor="skill-level">Skill Level:</label>
+              <input type="range" id="skill-level" name="skill-level" min="1" max="5" />
+
+              <button type="submit">Add Skill</button>
+            </form>
+          </div>
+        </div>
+      )}
+       {projectModal && (
+        <div className="modal">
+          <div className="modal__content">
+            <span className="close" onClick={handleCloseProject}>&times;</span>
             <h3>Add Skill</h3>
             <form>
               <label htmlFor="skill-name">Skill Name:</label>
