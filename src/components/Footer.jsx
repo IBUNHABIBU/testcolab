52e9613486d21c449d3c1d6a7c539326013072c0
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 14:46:37 +0300

    Style the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,15 @@ import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => (
   <footer className="footer">
-      <div className="footer__social">
-        <a href="https://github.com" target="_blank"><FaGithub /></a>
-        <a href="https://twitter.com" target="_blank"><FaTwitter /></a>
-        <a href="https://linkedin.com" target="_blank"><FaLinkedin /></a>
-      </div>
-      <div className="footer__contact">
-        <span>Phone: 555-555-5555</span>
-      </div>
-    </footer>
+    <div className="footer__social">
+      <a href="https://github.com/IBUNHABIBU" target="_blank" rel="noreferrer"><FaGithub /></a>
+      <a href="https://twitter.com/ibunhabibu" target="_blank" rel="noreferrer"><FaTwitter /></a>
+      <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+    </div>
+    <div className="footer__contact">
+      <span>Phone: 555-555-5555</span>
+    </div>
+  </footer>
 );
 
 export default Footer;
