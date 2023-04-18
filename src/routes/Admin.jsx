293ac7c4bf821027629b8c296f12e8322727d140
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 15:50:53 +0300

    Style the projects list

---
+++ src/routes/Admin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div>
+        <button>Add skills</button>
+        <button>Add Projects</button>
+    </div>
+  )
+}
+
+export default Admin
