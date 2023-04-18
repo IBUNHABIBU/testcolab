e4fe10795bedc0fb89d46434b13d90cc36f69215
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 15:12:46 +0300

    Style the footer

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const About = () => (
-  <div>About</div>
+  <div className="about">
+      <h1 className="about__title">Hi there, I'm Salum Habibu 👋</h1>
+      <p className="about__description">
+        I'm a remote Full-Stack Software developer and Electrical Engineer
+      </p>
+    </div>
 );
 
 export default About;
