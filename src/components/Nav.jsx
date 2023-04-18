2a4f9b72f75fb5599dced98b1000f7feaf4bf3d2
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 11:18:51 +0300

    Style the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,17 +7,19 @@ const Nav = () => (
     <div className="logo">
       <NavLink to="/" className="nav__link">Logo</NavLink>
     </div>
-    {
-      list.map((list) => (
-        <NavLink
-          to={list.path}
-          key={list.id}
-          className={({ isActive }) => (isActive ? 'nav__link active' : 'nav__link')}
-        >
-          {list.name}
-        </NavLink>
-      ))
-    }
+    <div className="nav__lists">
+      {
+        list.map((list) => (
+          <NavLink
+            to={list.path}
+            key={list.id}
+            className={({ isActive }) => (isActive ? 'nav__link active' : 'nav__link')}
+          >
+            {list.name}
+          </NavLink>
+        ))
+      }
+    </div>
   </div>
 );
 
