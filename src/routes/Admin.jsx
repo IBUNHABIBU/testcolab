5ebeb21e09ab329e77faa2b5673d9c2a501d8ab5
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-18 15:53:28 +0300

    Style the projects list

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const Admin = () => {
-  return (
-    <div>
-        <button>Add skills</button>
-        <button>Add Projects</button>
-    </div>
-  )
-}
+const Admin = () => (
+  <div className="admin">
+  <h2>Admin Panel</h2>
+  <div className="admin__buttons">
+    <button className="btn skills">Add Skill</button>
+    <button className="btn project">Add Project</button>
+  </div>
+  </div>
+);
 
-export default Admin
+export default Admin;
