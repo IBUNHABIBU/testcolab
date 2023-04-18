2401df7a89c45bcb0a6d3abdc3dde72a1e01d0c8
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-19 02:59:26 +0300

    Call the post request to create Project

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,4 +1,11 @@
-export const addCar = (data) => ({
-  type: actionTypes.ADD_CAR,
+import actionTypes from '../constants';
+
+export const addProject = (data) => ({
+  type: actionTypes.ADD_PROJECT,
   payload: data,
+});
+
+export const setUser = (user) => ({
+  type: actionTypes.SET_USER,
+  payload: user,
 });
--- src/redux/constants/index.js
+++ src/redux/constants/index.js
@@ -1,5 +1,6 @@
 const actionTypes = {
   ADD_PROJECT: 'ADD_PROJECT',
+  SET_USER: 'SET_USER',
 };
 
 export default actionTypes;
