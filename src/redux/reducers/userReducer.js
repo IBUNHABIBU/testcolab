3d850631a8e61b9d4a81f1f8f99fb78a17993b81
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-19 12:28:26 +0300

    Add user reducer

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,16 @@
+import actionTypes from '../constants/index';
+
+const initialState = {
+
+};
+
+const userReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case actionTypes.SET_USER:
+      return { ...state, ...payload };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
