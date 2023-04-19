d97dfbc19fd5bbe210c87daf00ce9acb1d289695
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-19 03:10:09 +0300

    Add project reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import projectReducer from './projectReducer';
+import skillsReducer from './skillsReducer';
 
 const reducers = combineReducers({
   project: projectReducer,
