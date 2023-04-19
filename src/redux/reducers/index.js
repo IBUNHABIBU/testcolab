546e6cac5e06fcb02c6741779df64923aec2a125
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-19 12:18:25 +0300

    Check admin

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import projectReducer from './projectReducer';
 import skillsReducer from './skillsReducer';
+import userReducer from './userReducer';
 
 const reducers = combineReducers({
   project: projectReducer,
   skills: skillsReducer,
+  user: userReducer,
 });
 
 export default reducers;
