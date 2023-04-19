a43db1198a1607a7e4c5305bad57861a48dd248f
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-19 14:58:05 +0300

    Add login code

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,11 +1,22 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+// import { useHistory } from 'react-router-dom';
+import {
+  redirect, useHistory,
+} from "react-router-dom";
 import { urlBase } from '../redux/actions/fetch';
 import { setUser } from '../redux/actions/actions';
 
 const Login = () => {
   const user = useSelector((state) => state.user);
+  const history = useHistory();
+  console.log('user', user.logged_in);
+  if (user.logged_in) {
+    // return redirect('/admin');
+    history.push('/admin');
+  }
+  
   const dispatch = useDispatch();
   const [errors, setErrors] = useState('');
   const [userInput, setuserInput] = useState({
@@ -13,7 +24,7 @@ const Login = () => {
     password: '',
   });
 
-  console.log("user", user)
+  console.log('user', user);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setuserInput((prevUserInput) => ({
@@ -34,7 +45,7 @@ const Login = () => {
       if (response.data.status === 'created') {
         dispatch(setUser(response.data));
       }
-      console.log("response", response)
+      console.log('response', response);
       setErrors(response.data.error);
     });
   };
