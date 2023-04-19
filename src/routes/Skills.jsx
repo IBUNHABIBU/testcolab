aa425dbe6ab0a83235deb044589202d275487c2f
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-19 21:24:15 +0300

    Display skills

--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchProjects } from '../redux/actions/fetch';
+import { fetchSkills } from '../redux/actions/fetch';
 
 const Skills = () => {
   const skills = useSelector((state) => state.skills);
@@ -13,13 +13,17 @@ const Skills = () => {
   return (
     <div className="skills">
       <h2 className="skills__title">Skills</h2>
-      <ul className="skills__list">
-        <li className="skills__list__item">HTML</li>
-        <li className="skills__list__item">CSS</li>
-        <li className="skills__list__item">JavaScript</li>
-        <li className="skills__list__item">React</li>
-        <li className="skills__list__item">Node.js</li>
-      </ul>
+      {
+        skills.map((skill) => {
+          const { id, name } = skill;
+          return (
+            <ul className="skills__list" key={id}>
+              <li className="skills__list__item">{name}</li>
+            </ul>
+          );
+        })
+      }
+
     </div>
   );
 };
