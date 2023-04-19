b71079e580da165a7abbbaa9dafead64f0449245
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-19 12:59:18 +0300

    Add login code

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { urlBase } from '../redux/actions/fetch';
 import { setUser } from '../redux/actions/actions';
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const [errors, setErrors] = useState('');
   const [userInput, setuserInput] = useState({
     email: '',
     password: '',
@@ -15,26 +18,27 @@ const Login = () => {
       [name]: value,
     }));
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      axios.post(`${urlBase}/sessions`, {
-        user: {
-          email: userInput.email,
-          password: userInput.password,
-        },
-      },
-      { withCredentials: true }).then((response) => {
-        if (response.data.status === 'created') {
-          dispatch(setUser(response.data));
-        }
-        setErrors(response.data.error);
-      });
-    };
   };
-  (
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post(`${urlBase}/sessions`, {
+      user: {
+        email: userInput.email,
+        password: userInput.password,
+      },
+    },
+    { withCredentials: true }).then((response) => {
+      if (response.data.status === 'created') {
+        dispatch(setUser(response.data));
+      }
+      setErrors(response.data.error);
+    });
+  };
+  return (
     <div>
       <h1 align="center">Login</h1>
-      <form className="form" onSubmit={handleSumit}>
+      <form className="form" onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
         <input type="text" id="email" name="email" onChange={handleChange} />
         <label htmlFor="password">Password:</label>
