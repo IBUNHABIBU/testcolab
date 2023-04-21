5e5ac9edce26b98aa8c05b60536c2f11e523573c
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-21 22:13:07 +0300

    Fix the lint errors

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const About = () => (
   <div className="about">
-    <h1 className="about__title">Hi there, I'm Salum Habibu 👋</h1>
+    <h1 className="about__title">Hi there, I am Salum Habibu 👋</h1>
     <p className="about__description">
-      I'm a remote Full-Stack Software developer and Electrical Engineer
+      I am a remote Full-Stack Software developer and Electrical Engineer
     </p>
   </div>
 );
