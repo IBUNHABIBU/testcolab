630052fbe46dd44c4b71f7d223c2121ee690a515
IBUNHABIBU <ibunhabib@gmail.com>
2023-04-21 22:47:11 +0300

    Fix the lint errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa';
 const Footer = () => (
   <footer className="footer">
     <div className="footer__social">
-      <a href="https://github.com/IBUNHABIBU" target="_blank" rel="noreferrer"><FaGithub /></a>
-      <a href="https://twitter.com/ibunhabibu" target="_blank" rel="noreferrer"><FaTwitter /></a>
-      <a href="https://linkedin.com/in/salum-habibu" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+      <a href="https://github.com/IBUNHABIBU" target="_blank" rel="noreferrer" aria-label="GitHub"><FaGithub /></a>
+      <a href="https://twitter.com/ibunhabibu" target="_blank" rel="noreferrer" aria-label="Twitter"><FaTwitter /></a>
+      <a href="https://linkedin.com/in/salum-habibu" target="_blank" rel="noreferrer" aria-label="LinkedIn"><FaLinkedin /></a>
     </div>
     <div className="footer__contact">
       <span>Phone: +255-752-443624</span>
